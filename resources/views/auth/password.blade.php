7b2bf0353c5e912b92915cca09fac8358001e550
Matt Trimarchi <matt@Matts-iMac.local>
2015-05-07 00:25:52 +1000

    front pages

--- resources/views/auth/password.blade.php
+++ resources/views/auth/password.blade.php
@@ -1,50 +1,60 @@
-@extends('app')
+@extends('frontend')
 
 @section('content')
-<div class="container-fluid">
-	<div class="row">
-		<div class="col-md-8 col-md-offset-2">
-			<div class="panel panel-default">
-				<div class="panel-heading">Reset Password</div>
-				<div class="panel-body">
-					@if (session('status'))
-						<div class="alert alert-success">
-							{{ session('status') }}
-						</div>
-					@endif
 
-					@if (count($errors) > 0)
-						<div class="alert alert-danger">
-							<strong>Whoops!</strong> There were some problems with your input.<br><br>
-							<ul>
-								@foreach ($errors->all() as $error)
-									<li>{{ $error }}</li>
-								@endforeach
-							</ul>
-						</div>
-					@endif
+ <section id="form">
 
-					<form class="form-horizontal" role="form" method="POST" action="{{ url('/password/email') }}">
-						<input type="hidden" name="_token" value="{{ csrf_token() }}">
+        <div class="row">
 
-						<div class="form-group">
-							<label class="col-md-4 control-label">E-Mail Address</label>
-							<div class="col-md-6">
-								<input type="email" class="form-control" name="email" value="{{ old('email') }}">
-							</div>
-						</div>
+            <div class="column medium-6 medium-centered">
 
-						<div class="form-group">
-							<div class="col-md-6 col-md-offset-4">
-								<button type="submit" class="btn btn-primary">
-									Send Password Reset Link
-								</button>
-							</div>
-						</div>
-					</form>
-				</div>
-			</div>
-		</div>
-	</div>
-</div>
+            <h3>Reset Password</h3>
+
+
+            @if (session('status'))
+                                    <div class="alert-box success radius">
+                                        {{ session('status') }}
+                                    </div>
+                                @endif
+
+            @if (count($errors) > 0)
+                                    <div class="alert-box radius alert">
+                                        <strong>Whoops!</strong> There were some problems with your input.<br><br>
+                                        <ul>
+                                            @foreach ($errors->all() as $error)
+                                                <li>{{ $error }}</li>
+                                            @endforeach
+                                        </ul>
+                                    </div>
+                                @endif
+
+
+            <form class="form-horizontal" role="form" method="POST" action="{{ url('/password/email') }}">
+                                    <input type="hidden" name="_token" value="{{ csrf_token() }}">
+
+        <div class="row">
+            <div class="large-12 columns">
+              <label>Email Address
+                <input type="email" class="form-control" name="email" value="{{ old('email') }}">
+              </label>
+            </div>
+          </div>
+
+          <div class="row">
+                      <div class="large-12 columns">
+                       <button type="submit" class="button radius small">
+                                                           Send Password Reset Link
+                                                       </button>
+                      </div>
+                    </div>
+
+
+            </form>
+
+            </div>
+
+        </div>
+
+    </section>
+
 @endsection
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,77 +1,86 @@
-@extends('app')
+@extends('frontend')
 
 @section('content')
-    <div class="container-fluid">
-        <div class="row">
-            <div class="col-md-8 col-md-offset-2">
-                <div class="panel panel-default">
-                    <div class="panel-heading">Register</div>
-                    <div class="panel-body">
-                        @if (count($errors) > 0)
-                            <div class="alert alert-danger">
-                                <strong>Whoops!</strong> There were some problems with your input.<br><br>
-                                <ul>
-                                    @foreach ($errors->all() as $error)
-                                        <li>{{ $error }}</li>
-                                    @endforeach
-                                </ul>
-                            </div>
-                        @endif
 
-                        <form class="form-horizontal" role="form" method="POST" action="{{ url('/auth/register') }}">
-                            <input type="hidden" name="_token" value="{{ csrf_token() }}">
+    <section id="form">
 
-                            <div class="form-group">
-                                <label class="col-md-4 control-label">Name</label>
+           <div class="row">
 
-                                <div class="col-md-6">
-                                    <input type="text" class="form-control" name="name" value="{{ old('name') }}">
-                                </div>
-                            </div>
+               <div class="column medium-6 medium-centered">
 
-                            <div class="form-group">
-                                <label class="col-md-4 control-label">E-Mail Address</label>
+               <h3>Register</h3>
 
-                                <div class="col-md-6">
-                                    <input type="email" class="form-control" placeholder="you@example.com" name="email" value="{{ old('email') }}">
-                                </div>
-                            </div>
+              @if (count($errors) > 0)
+                                          <div class="alert-box radius alert">
+                                              <strong>Whoops!</strong> There were some problems with your input.<br><br>
+                                              <ul>
+                                                  @foreach ($errors->all() as $error)
+                                                      <li>{{ $error }}</li>
+                                                  @endforeach
+                                              </ul>
+                                          </div>
+                                      @endif
 
-                            <div class="form-group">
-                                <label class="col-md-4 control-label">Domain</label>
 
-                                <div class="col-md-6">
-                                    <input type="text" class="form-control" placeholder="example.com" name="url" value="{{ old('url') }}">
-                                </div>
-                            </div>
+               <form class="form-horizontal" role="form" method="POST" action="{{ url('/auth/register') }}">
+                                           <input type="hidden" name="_token" value="{{ csrf_token() }}">
 
-                            <div class="form-group">
-                                <label class="col-md-4 control-label">Password</label>
+               <div class="row">
+                   <div class="large-12 columns">
+                     <label>Your Name
+                       <input type="text" class="form-control" name="name" value="{{ old('name') }}">
+                     </label>
+                   </div>
+                 </div>
 
-                                <div class="col-md-6">
-                                    <input type="password" class="form-control" name="password">
-                                </div>
-                            </div>
 
-                            <div class="form-group">
-                                <label class="col-md-4 control-label">Confirm Password</label>
+                 <div class="row">
+                                    <div class="large-12 columns">
+                                      <label>Email Address
+                                        <input type="email" class="form-control" placeholder="you@example.com" name="email" value="{{ old('email') }}">
+                                      </label>
+                                    </div>
+                                  </div>
 
-                                <div class="col-md-6">
-                                    <input type="password" class="form-control" name="password_confirmation">
-                                </div>
-                            </div>
 
-                            <div class="form-group">
-                                <div class="col-md-6 col-md-offset-4">
-                                    <button type="submit" class="btn btn-primary">
-                                        Register
-                                    </button>
-                                </div>
-                            </div>
-                        </form>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
+
+                                  <div class="row">
+                                                     <div class="large-12 columns">
+                                                       <label>Your Website
+                                                        <input type="text" class="form-control" placeholder="example.com" name="url" value="{{ old('url') }}">
+                                                       </label>
+                                                     </div>
+                                                   </div>
+
+                                                   <div class="row">
+                                                                      <div class="large-12 columns">
+                                                                        <label>Password
+                                                                          <input type="password" class="form-control" name="password">
+                                                                        </label>
+                                                                      </div>
+                                                                    </div>
+
+                                                                     <div class="row">
+                                                                        <div class="large-12 columns">
+                                                                        <label>Confirm Password
+                                                                        <input type="password" class="form-control" name="password_confirmation">
+                                                                        </label>
+                                                                        </div>
+                                                                        </div>
+
+
+ <div class="row">
+                                    <div class="large-12 columns">
+                                      <button type="submit" class="button radius small">Register</button>
+                                    </div>
+                                  </div>
+
+               </form>
+
+               </div>
+
+           </div>
+
+       </section>
+
 @endsection
